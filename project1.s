0a5ffa703a415b1dd3ff5fce761529a7d62c2735
Bikatr7 <Tetralon07@gmail.com>
2024-03-02 15:40:59 -0700

    added write_to terminal

--- project1.s
+++ project1.s
@@ -7,39 +7,41 @@
 
 .text
 main:
-	# main() prolog
-	addi sp, sp, -104
-	sw ra, 100(sp)
+    ## main() prolog
+    addi sp, sp, -104
+    sw ra, 100(sp)
 
-	# main() body
+    ## main() body
 
-	# Write the prompt to the terminal (stdout)
-	li a7, __NR_WRITE
-	li a0, STDOUT
-	la a1, prompt
-	addi  a2, zero, prompt_end - prompt        
-	ecall
+    ## Call write function to write the prompt to the terminal (stdout)
+    la a1, prompt
+    addi  a2, zero, prompt_end - prompt
+    call write_to_terminal
 
-    # Read up to 100 characters from the terminal (stdin)
+    ## Call read function to read up to 100 characters from the terminal (stdin)
     call read_from_terminal
 
-	# Write the just read characters to the terminal (stdout)
-	addi a2, a0, 0
-	li a7, __NR_WRITE
-	li a0, STDOUT
-	mv a1, sp
-	ecall
+    ## Call write function to write the just read characters to the terminal (stdout)
+    addi a2, a0, 0
+    call write_to_terminal
 
-	# main() epilog
-	lw ra, 100(sp)
-	addi sp, sp, 104
-	ret
+    ## main() epilog
+    lw ra, 100(sp)
+    addi sp, sp, 104
+    ret
 
 read_from_terminal:
     li a7, __NR_READ
     li a0, STDIN
     mv a1, sp
     addi a2, zero, 100
+    ecall
+    ret
+
+write_to_terminal:
+    # Write to the terminal (stdout)
+    li a7, __NR_WRITE
+    li a0, STDOUT
     ecall
     ret
 
