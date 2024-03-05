3694698adae39c2c575fd5105237e20b24716c2b
Bikatr7 <Tetralon07@gmail.com>
2024-03-05 03:36:46 -0700

    holy shit it works

--- Project 1/project1-libc.s
+++ Project 1/project1-libc.s
@@ -1,83 +1,79 @@
-.globl main
-.equ STDOUT, 1
 .equ STDIN, 0
+.equ STDOUT, 1
 .equ __NR_READ, 63
 .equ __NR_WRITE, 64
-.equ __NR_EXIT, 93
 
 .text
+
+.globl main
+
 main:
-    # Main prolog
-    addi sp, sp, -104
-    sw ra, 100(sp)
+    la a0, prompt     # Load address of prompt message
 
-    # Call puts to write the prompt
-    la a0, prompt
-    call puts
+    call puts         
+    la a0, buf        # Load address of buffer
 
-    # Call gets to read input
-    la a0, buf
-    call gets
+    call gets         
+    la a0, buf        # Load address of buffer
 
-    # Call puts to echo the input
-    la a0, buf
-    call puts
-
-    # Main epilog
-    lw ra, 100(sp)
-    addi sp, sp, 104
+    call puts       
     ret
 
-putchar:
-    li a7, __NR_WRITE
-    li a0, STDOUT
-    addi a1, sp, 12 
-    sw a0, 0(a1)
-    li a2, 1
-    ecall
-    lw a0, 0(a1)
+# Argument: a0 = const char* s
+puts:
+    mv t0, a0         # Save the start address of the string
+
+puts_loop:
+    lb a1, 0(t0)      # Load the next byte of the string
+    beqz a1, done_puts # If the byte is 0, end of string
+    call putchar     
+    addi t0, t0, 1    # Move to the next character
+    j puts_loop       
+
+done_puts:
+    li a1, 10       # Output a newline at the end
+    call putchar
+    li a0, 0          # Return 0 on success
+    ret
+
+# Argument: a0 = char* s
+gets:
+    mv t1, a0         # Save the start address of the buffer
+
+gets_loop:
+    call getchar      # Read a character
+    sb a0, 0(t1)      # Store the character in the buffer
+    beq a0, 10, done_gets # If the character is newline, stop
+    addi t1, t1, 1    # Increment buffer pointer
+    j gets_loop       
+done_gets:
+    sb zero, 0(t1)    # Null-terminate the string
     ret
 
 getchar:
-    li a7, __NR_READ
-    li a0, STDIN
-    addi a1, sp, 12 
-    li a2, 1
-    ecall
-    lb a0, 0(a1) 
+    li a7, __NR_READ  
+    li a0, STDIN     
+    addi sp, sp, -4   # Allocate space on the stack
+    mv a1, sp         # Pointer to the stack space
+    li a2, 1          # Read one character
+    ecall            
+    lb a0, 0(sp)      # Load the read character
+    addi sp, sp, 4    # Deallocate stack space
     ret
 
-gets:
-    addi t0, a0, 0 
-
-getchar_loop:
-    call getchar
-    sb a0, 0(t0) ## Store read character
-    addi t0, t0, 1 ## Increment buff point
-    li t1, 10
-    beq a0, t1, finish_gets ## Break if newline
-    j getchar_loop
-
-finish_gets:
-    sb zero, -1(t0) ## Null-term string
-    ret
-
-puts:
-    addi t0, a0, 0 ## Copy string address to t0
-
-puts_loop:
-    lb a0, 0(t0) ## Load character
-    beq a0, zero, finish_puts ## Break if its null-terminator
-    call putchar
-    addi t0, t0, 1 ## Increments string pointer
-    j puts_loop
-finish_puts:
-
-    li a0, 10
-    call putchar ## Write newline
+# Argument: a1 = int c
+putchar:
+    li a7, __NR_WRITE 
+    li a0, STDOUT    
+    addi sp, sp, -4   # Allocate space on the stack
+    sb a1, 0(sp)      # Store the character on the stack
+    mv a1, sp         # Pointer to the character
+    li a2, 1          # Write one character
+    ecall             
+    addi sp, sp, 4    # Deallocate stack space
+    mv a0, a1         # Return the character written
     ret
 
 .data
 prompt: .asciz "Enter a message: "
-prompt_end:
-buf: .space 100
+buf:    .space 100
